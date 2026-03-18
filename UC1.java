b6b14ee539e53a49d295be56f3b5a46e486c67c0
samridhigangwar235 <samridhigangwar235@gmail.com>
2026-03-18 19:24:37 +0530

    Coded OOPS

--- UC1.java
+++
@@ -1,9 +0,0 @@
-public class UC1 {// Define the class
-
-    // Entry point of the application
-    public static void main(String[] args) {
-        // Print "OOPS" to the console
-        System.out.println("OOPS");
-    }
-}
-
---
+++ UC2.java
@@ -0,0 +1,20 @@
+public class UC2 {
+    //uc2
+    public static void main(String[] args) {
+        
+    System.out.println("     ***      "  + " " + "     ***      " +" " +"****** " + "   "+ "  *******");
+    
+    System.out.println("  **     **   "  + " " + "  **     **   " +" " +"**    **"+"   "+ "**");
+
+    System.out.println("**         ** "  + " " + "**         ** " +" " +"**    **"+"   "+"**");
+
+    System.out.println("**         ** "  + " " + "**         ** " +" " +"*****"+ " "+ "     ******** ");
+
+    System.out.println("**         ** "  + " " + "**         ** " +" " +"***"+ " "+ "             **");
+
+    System.out.println(" **       **  "  + " " + " **       **  " +" " +"***"+ " "+ "            **");
+
+    System.out.println("     ***      "  + " " + "     ***      " +" "+"***"+ " "+  "      ********");
+    }
+}
+
