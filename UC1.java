f697341abe45ac1a93f22d53fa3fb55929138871
samridhigangwar235 <samridhigangwar235@gmail.com>
2026-03-18 19:23:15 +0530

    Coded OOPS

---
+++ UC1.java
@@ -0,0 +1,9 @@
+public class UC1 {// Define the class
+
+    // Entry point of the application
+    public static void main(String[] args) {
+        // Print "OOPS" to the console
+        System.out.println("OOPS");
+    }
+}
+
